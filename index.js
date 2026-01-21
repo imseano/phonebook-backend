034da6d10b2123a148459e2b089e7e84be423fba
Sean Oreta <spatrickoreta@gmail.com>
2026-01-21 01:07:20 -0800

    feat: delete persons request

--- index.js
+++ index.js
@@ -45,6 +45,11 @@ app.get('/info', (req, res) => {
     res.send('<div>' + statement + dateStatement + '</div>')
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
+})
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server runnning on port ${PORT}`)
